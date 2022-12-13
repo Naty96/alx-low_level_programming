ca6909f368a0faa49c64bdd6ff8124fa7f9af769
Naty96 <belewnathnael@gmail.com>
2022-12-13 00:47:38 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints all possible different combinations of two digits
@@ -12,11 +12,11 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-	
+
 	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
-		{
+	{
 			if (!((ones == tens) || (tens > ones)))/*eliminates repition*/
 			{
 				putchar(tens);
@@ -29,9 +29,7 @@ int main(void)
 			}
 		}
 	}
-	
 	putchar('\n');
 
 	return (0);
-
 }
