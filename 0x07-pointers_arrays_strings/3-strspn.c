fea830ae12de79b8b249349b6688b0d627ab30a8
Naty96 <belewnathnael@gmail.com>
2023-01-03 08:45:00 -0800

    3rd task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,6 @@ unsigned int _strspn(char *s, char *accept)
 
 	while (s[i] != '\0') /*iterate through string*/
 	{
-
 		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
 		{
 			if (s[i] == accept[j]) /*record & break at first match*/
@@ -21,15 +20,10 @@ unsigned int _strspn(char *s, char *accept)
 				matches++;
 				break;
 			}
-
 			if (accept[j + 1] == '\0' && s[i] != accept[j])
-
-			return (matches);/*return if idx doesn't match*/
+				return (matches);/*return if idx doesn't match*/
 		}
-
 		i++;
 	}
-
 	return (matches); /* return num if all match till end */
-
 }
